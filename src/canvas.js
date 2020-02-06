546e1313dacccfb423b905c393121e0459c0468c
unknown <ricardotomaz027@gmail.com>
2020-02-06 12:42:28 +0000

    done

--- src/canvas.js
+++ src/canvas.js
@@ -6,33 +6,59 @@ class Canvas {
     this.positionX_keep = 25;
     this.positionX_going = 50;
     this.audio = new Audio('./audio/bullet.mp3');
+    this.hue = 0;
   }
   clear() {
     this.ctx.clearRect(0, 0, 150, 150);
   }
 
-  updateCanvas() {
-    let hue = Math.random() * 359;
-    let color = 'hsl(' + hue + ',100%,50%)';
-    this.ctx.fillStyle = color;
-    this.ctx.font = '40px serif';
-    this.ctx.fillText('Keep', this.positionX_keep, 50);
-    this.ctx.fillText('Going', this.positionX_going, 80);
-    this.audio.play();
+  updateHue() {
+    if (this.hue < 359) {
+      this.hue += 10;
+      console.log(this.hue);
+    } else if (this.hue == 360) {
+      this.hue *= -1;
+      console.log(this.hue);
+    } else {
+      this.hue += 10;
+      console.log(this.hue);
+    }
+  }
 
-    setTimeout(() => {
-      this.hidden = true;
-      this.ctx.clearRect(0, 0, 150, 150);
-    }, 3000);
+  updateCanvas() {
+    console.log('Hello World');
+    if (this.hidden === false) {
+      let color = 'hsl(' + Math.abs(this.hue) + ',100%,50%)';
+      this.audio.play();
+      this.ctx.fillStyle = color;
+
+      this.ctx.font = '40px serif';
+      this.ctx.fillText('Keep', this.positionX_keep, 50);
+      this.ctx.fillText('Going', this.positionX_going, 80);
+
+      setTimeout(() => {
+        this.hidden = true;
+
+        this.ctx.fillStyle = color;
+        this.clear();
+      }, 3000);
+    } else {
+    }
   }
 
   paint() {
     this.updateCanvas();
+    this.updateHue();
   }
 
   loop() {
     if (!this.hidden) {
-      this.paint();
+      setTimeout(() => {
+        this.updateHue();
+        this.paint();
+        this.loop();
+      }, 100);
+    } else {
     }
   }
 }
